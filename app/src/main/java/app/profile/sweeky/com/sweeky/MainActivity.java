7a74d04b6da6481aa6ea4ae91d563a655a7351b3
haseebpvt <haseebpvt@gmail.com>
2018-09-01 01:36:02 +0530

    Initial commit

---
+++ app/src/main/java/app/profile/sweeky/com/sweeky/MainActivity.java
@@ -0,0 +1,13 @@
+package app.profile.sweeky.com.sweeky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
