792a92123835b340517cbc5a551f0b590f0e9d67
haseebpvt <haseebpvt@gmail.com>
2018-09-08 00:23:47 +0530

    Login user details collection

---
+++ app/src/main/java/app/profile/sweeky/com/sweeky/Const/Firebase/Firestore.java
@@ -0,0 +1,8 @@
+package app.profile.sweeky.com.sweeky.Const.Firebase;
+
+public class Firestore {
+
+    public static final String COLLECTION_USERS = "Users";
+    public static final String DOCUMENT_USER_DETAILS = "user_details";
+
+}
